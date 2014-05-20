4cd96b2e05a3fdc8bce701f5e95678a78dac2bd1
pudgecon <pudgecon@gmail.com>
2014-05-20 22:43:44 +0800

    API for articles and article_nodes

--- app/controllers/admin/article_nodes_controller.rb
+++ app/controllers/admin/article_nodes_controller.rb
@@ -6,14 +6,42 @@ class Admin::ArticleNodesController < ApplicationController
   end
 
   def show
+    article_node = ArticleNode.params[:id]
+
+    if article_node
+      render json: article_node, serializer: ArticleNodeSerializer, status: :created
+    else
+      head :no_found
+    end
   end
 
   def create
+    article_node = ArticleNode.create! resource_params
+
+    render json: article_node, serializer: ArticleNodeSerializer, status: :created
   end
 
   def update
+    article_node = ArticleNode.find params[:id]
+
+    if article_node.update_attributes resource_params
+      render json: article_node, serializer: ArticleNodeSerializer, status: :ok
+    else
+      render json: { errors: article_nodes.errors }, status: :unprocess_entity
+    end
   end
 
   def destroy
+    article_node = ArticleNode.params[:id]
+
+    if article_node.destroy
+      head :ok
+    else
+      render json: { errors: article_nodes.errors }, status: :unprocess_entity
+    end
+  end
+
+  def resource_params
+    params.require(:article_node).permit :id, :node_text, :node_type, :article_id
   end
 end
--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,31 +1,43 @@
-module Admin
-  class ArticlesController < ApplicationController
-    def index
-      articles = Article.all
+class Admin::ArticlesController < ApplicationController
+  def index
+    articles = Article.all
 
-      render json: articles, each_serializer: ArticleSerializer, status: :ok
-    end
+    render json: articles, each_serializer: ArticleSerializer, status: :ok
+  end
 
-    def show
-      article = Article.find params[:id]
+  def show
+    article = Article.find params[:id]
 
+    render json: article, serializer: ArticleSerializer, status: :ok
+  end
+
+  def create
+    article = Article.create! resource_params
+
+    render json: article, serializer: ArticleSerializer, status: :created
+  end
+
+  def update
+    article = Article.find params[:id]
+
+    if article.update_attributes resource_params
       render json: article, serializer: ArticleSerializer, status: :ok
+    else
+      render json: { errors: article.errors }, status: :unprocess_entity
     end
+  end
 
-    def create
-      article = Article.create! resource_params
+  def destroy
+    article = Article.params[:id]
 
-      render json: article, serializer: ArticleSerializer, status: :created
+    if article.destroy
+      head :ok
+    else
+      render json: { errors: article.errors }, status: :unprocess_entity
     end
+  end
 
-    def update
-    end
-
-    def destroy
-    end
-
-    def resource_params
-      params.require(:article).permit :id, :title, :author
-    end
+  def resource_params
+    params.require(:article).permit :id, :title, :author
   end
 end
